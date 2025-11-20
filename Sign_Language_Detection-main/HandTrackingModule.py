6da6a55b46a8eba1437226267b4ec3f469ad2286
RachitAgrawal04 <agrawalanush4@gmail.com>
2025-11-20 10:19:20 +0530

    commit by ritik

--- Sign_Language_Detection-main/HandTrackingModule.py
+++ Sign_Language_Detection-main/HandTrackingModule.py
@@ -9,14 +9,14 @@ class handDetector():
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
-        self.mpHands = mp.solutions.hands
+        self.mpHands = mp.solutions.hands  # type: ignore
         self.hands = self.mpHands.Hands(
             static_image_mode=self.mode,
             max_num_hands=self.maxHands,
             min_detection_confidence=self.detectionCon,
             min_tracking_confidence=self.trackCon,
         )
-        self.mpDraw = mp.solutions.drawing_utils
+        self.mpDraw = mp.solutions.drawing_utils  # type: ignore
         self.result = None
 
     def findHands(self, img, draw = True) :
